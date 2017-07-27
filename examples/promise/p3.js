86f1861964fe815e1c1114db865f418b0bab4cc3
ggm <godgunman@gmail.com>
2017-07-27 20:53:13 +0800

    update

---
+++ examples/promise/p3.js
@@ -0,0 +1,24 @@
+const get2 = (number, done) => {
+  setTimeout(() => {
+    done(number * number)
+  }, 2000)
+}
+
+const get2p = (number) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      res(number * number)
+    }, 2000)
+  });
+}
+
+const get5 = (number, done) => {
+  setTimeout(() => {
+    done(number * number * number * number * number)
+  }, 5000)
+}
+const get7 = (number, done) => {
+  setTimeout(() => {
+    done(number * number * 0.7)
+  }, 7000)
+}
