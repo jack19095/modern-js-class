26e7856d214cb428103233748d7f4440337e69b5
godgunman <godgunman@gmail.com>
2017-10-30 20:30:42 +0800

    Update

--- examples/promise/request-example/app-promise2.js
+++ examples/promise/request-example/app-promise2.js
@@ -1,27 +1,27 @@
-const rp = require("request-promise");
-const getInfo = require("./getInfo");
+const rp = require("request-promise")
+const getInfo = require("./getInfo")
 
 const main = address => {
-  address = encodeURI(address);
-  let addressUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`;
+  address = encodeURI(address)
+  let addressUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`
 
   rp(addressUrl)
     .then(body => {
-      let { lat, lng } = getInfo(JSON.parse(body));
-      let location = `${lat},${lng}`;
-      let placeAPI = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=500&type=restaurant&key=AIzaSyBImOy7k7q3nRG0YOcN2Z4GfQDu3q7WYNE`;
+      let { lat, lng } = getInfo(JSON.parse(body))
+      let location = `${lat},${lng}`
+      let placeAPI = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=500&type=restaurant&key=AIzaSyBImOy7k7q3nRG0YOcN2Z4GfQDu3q7WYNE`
       return rp(placeAPI)
     })
     .then(body => {
-      body = JSON.parse(body);
+      body = JSON.parse(body)
       let places = body.results.map(element => {
         return {
           name: element.name,
           rating: element.rating
-        };
-      });
-      console.log(places);
+        }
+      })
+      console.log(places)
     })
-};
+}
 
-main("台灣大學");
+main("台灣大學")
