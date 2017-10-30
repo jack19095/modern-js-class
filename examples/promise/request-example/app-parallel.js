9f5476c57845e0251e401a679b1147ad9d6c2cda
godgunman <godgunman@gmail.com>
2017-10-30 21:09:36 +0800

    Add app-parallel

---
+++ examples/promise/request-example/app-parallel.js
@@ -0,0 +1,35 @@
+const rp = require("request-promise")
+const getInfo = require("./getInfo")
+
+const KEY = 'AIzaSyBImOy7k7q3nRG0YOcN2Z4GfQDu3q7WYNE'
+
+const searchRestaurant = async address => {
+  address = encodeURI(address)
+  let addressUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${KEY}`
+
+  let addressBody = await rp(addressUrl)
+  let { lat, lng } = getInfo(JSON.parse(addressBody))
+  let location = `${lat},${lng}`
+  let placeUrl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=500&type=restaurant&key=${KEY}`
+
+  let placeBody = await rp(placeUrl)
+  placeBody = JSON.parse(placeBody)
+  let places = placeBody.results.map(element => {
+    return {
+      name: element.name,
+      rating: element.rating
+    }
+  })
+  return places
+}
+
+let addressList = [
+  '台灣大學', '國父紀念館', '台北101', '台北市政府'
+]
+
+for (let address of addressList) {
+  let result = searchRestaurant(address)
+  result.then((places) => {
+    console.log(address, places)
+  })
+}
