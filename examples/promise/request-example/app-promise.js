67d2f92a227d366498ec609bea948f8a7dd1943d
godgunman <godgunman@gmail.com>
2017-10-30 19:56:29 +0800

    Update

--- examples/promise/request-example/app-promise.js
+++ examples/promise/request-example/app-promise.js
@@ -1,42 +1,45 @@
-const request = require("request");
-const getInfo = require("./getInfo");
+const request = require("request")
+const getInfo = require("./getInfo")
 
 const main = address => {
-  address = encodeURI(address);
-  let addressUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`;
 
-  let addressPromise = new Promise((resolve, reject) => {
-    request(addressUrl, function (error, response, body) {
-      let { lat, lng } = getInfo(JSON.parse(body));
-      let location = `${lat},${lng}`;
-      resolve(location)
-    });
-  });
+  let getAddressPromise = (address) => {
+    address = encodeURI(address)
+    let addressUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`
+    let addressPromise = new Promise((resolve, reject) => {
+      request(addressUrl, function (error, response, body) {
+        let { lat, lng } = getInfo(JSON.parse(body))
+        let location = `${lat},${lng}`
+        resolve(location)
+      })
+    })
+    return addressPromise
+  }
 
   let getPlacePromise = (location) => {
     let placePromise = new Promise((resolve, reject) => {
-      let placeAPI = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=500&type=restaurant&key=AIzaSyBImOy7k7q3nRG0YOcN2Z4GfQDu3q7WYNE`;
+      let placeAPI = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location}&radius=500&type=restaurant&key=AIzaSyBImOy7k7q3nRG0YOcN2Z4GfQDu3q7WYNE`
       request(placeAPI, function (error, response, body) {
-        body = JSON.parse(body);
+        body = JSON.parse(body)
         let places = body.results.map(element => {
           return {
             name: element.name,
             rating: element.rating
-          };
-        });
-        resolve(places);
-      });
+          }
+        })
+        resolve(places)
+      })
     })
-    return placePromise;
-  };
+    return placePromise
+  }
 
-  addressPromise
+  getAddressPromise(address)
     .then((location) => {
       return getPlacePromise(location)
     })
     .then((places) => {
       console.log(places)
     })
-};
+}
 
-main("台灣大學");
+main("台灣大學")
